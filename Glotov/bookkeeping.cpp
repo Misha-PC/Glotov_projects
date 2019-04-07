39bf46f66248dc850b896c8e6c9cbdd9a2262949
Misha-PC <misha.rus.ru@gmail.com>
2019-04-07 09:40:21 +0300

    Add destroyer and constuctor

--- Glotov/bookkeeping.cpp
+++ Glotov/bookkeeping.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include "bookkeeping.h"
 
+Bookkeeping::Bookkeeping(std::string name, int age, double money)
+{
+  Bookkeeping::setMeta(name, age, money);
+  std::cout << name << "\tinitialized. \n";
+}
+
+Bookkeeping::~Bookkeeping()
+{
+  std::cout << m_name <<"\tdestroyed\n";
+}
+
+
 void Bookkeeping::print()
 {
   std::cout << "Name: " << m_name <<
@@ -9,10 +21,18 @@ void Bookkeeping::print()
            "\nMoney: $" << m_money << '\n';
 }
 
-std::string Bookkeeping::getName()            {  return(m_name);  }
-double Bookkeeping::getMoney()                { return(m_money); }
-int Bookkeeping::getAge()                     { return(m_age); }
 
-void Bookkeeping::setName(std::string name)   {m_name = name;}
-void Bookkeeping::setAge(int age)             {  m_age = age;}
-void Bookkeeping::setMoney(int money)         {  m_money = money;}
+std::string Bookkeeping::getName()            { return(m_name);   }
+double Bookkeeping::getMoney()                { return(m_money);  }
+int Bookkeeping::getAge()                     { return(m_age);    }
+
+void Bookkeeping::setName(std::string name)   {  m_name = name;   }
+void Bookkeeping::setAge(int age)             {  m_age = age;     }
+void Bookkeeping::setMoney(int money)         {  m_money = money; }
+
+void Bookkeeping::setMeta(std::string name, int age, double money)
+{
+  Bookkeeping::setName(name);
+  Bookkeeping::setAge(age);
+  Bookkeeping::setMoney(money);
+}
--- Glotov/bookkeeping.h
+++ Glotov/bookkeeping.h
@@ -4,20 +4,26 @@
 #ifndef BOOKKEEPING_H
 #define BOOKKEEPING_H
 
-class Bookkeeping   {
+class Bookkeeping
+{
 public:
   std::string m_name;
   int m_age;
   double m_money;
 
+  Bookkeeping(std::string, int, double);
+  ~Bookkeeping();
+
   void print();
+
+  std::string getName();
+  int getAge();
+  double getMoney();
 
   void setName(std::string name);
   void setAge(int age);
   void setMoney(int money);
 
-  std::string getName();
-  int getAge();
-  double getMoney();
+  void setMeta(std::string, int, double);
 };
 #endif
